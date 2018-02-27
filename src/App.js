6aaf22d27de0b5d243ec0b19fed48a811a3bddc3
Bill Buchanan <bbuchanan@gmail.com>
2018-02-27 09:39:53 -0500

    oooo ... a change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -11,7 +11,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This app is under continuous integration and deployment. Start writing
+          some tests!
         </p>
       </div>
     );
